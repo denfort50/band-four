20143749dd44fedf212dea27f37e8390594a88c0
Denis <i@dkalchenko.ru>
2025-11-03 14:01:14 +0300

    Фасад

---
+++ src/main/java/ru/dkalchenko/structural/facade/SprintRunner.java
@@ -0,0 +1,8 @@
+package ru.dkalchenko.structural.facade;
+
+public class SprintRunner {
+    public static void main(String[] args) {
+        WorkFlow workFlow = new WorkFlow();
+        workFlow.solveProblems();
+    }
+}
